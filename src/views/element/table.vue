8c395a9d1661c1b9209513b461edfd9fd8bb10fe
thatu <that_u@163.com>
2019-06-21 00:18:20 +0800

    交

--- src/views/element/table.vue
+++ src/views/element/table.vue
@@ -1,5 +1,45 @@
 <template>
   <div class="table">
+    <card>
+      <el-table
+        border
+        :data="list"
+        v-loading="loading"
+        style="width: 100%">
+        <el-table-column
+          type="index">
+        </el-table-column>
+        <el-table-column
+          prop="title"
+          label="标题"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="company"
+          label="公司"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          label="照片">
+          <template slot-scope="scope">
+            <a :href="scope.row.photo">{{ scope.row.photo }}</a>
+          </template>
+        </el-table-column>
+        <el-table-column
+          prop="attention_degree"
+          label="学位">
+        </el-table-column>
+      </el-table>
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="currentPage"
+        :page-sizes="[10,20,30]"
+        :page-size="pageSizes"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="this.lists.length">
+      </el-pagination>
+    </card>
   </div>
 </template>
 
@@ -9,12 +49,51 @@ import axios from 'axios'
 export default {
   data() {
     return {
-      lists: []
+      lists: [],
+      list: [],
+      loading: false,
+      currentPage: 1,
+      pageSizes: 10,
     }
   },
   created(){
+    this.getList()
+  },
+  mounted() {
+    this.tableHeight = `${document.body.clientHeight - 100}px`
   },
   methods: {
+    getList() {
+      this.loading = true
+      axios.get('/api/data')
+      .then((res) => {
+        if(res.data.code === 200){
+          this.lists = res.data.data
+          this.loading = false
+          this.init()
+        }
+      })
+      .catch((res) => {
+        console.log(error);
+      })
+    },
+    init(){
+     this.list =  this.lists.filter((ele, index) => {
+        if(index >= (this.currentPage - 1) *  this.pageSizes && index < this.currentPage * this.pageSizes) {
+          return  ele
+        }
+      })
+    },
+    handleSizeChange(val) {
+      // console.log(`每页 ${val} 条`);this
+      this.pageSizes = val
+      this.init()
+    },
+    handleCurrentChange(val) {
+      // console.log(`当前页: ${val}`);
+      this.currentPage = val
+      this.init()
+    }
   }
 }
 /**
@@ -25,4 +104,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+a {
+  text-decoration: none;
+}
 </style>
