d5e57bea60a92b285ce387deb49168894b1b39f3
thatu <that_u@163.com>
2019-07-04 23:23:37 +0800

    暂存

--- src/views/element/table.vue
+++ src/views/element/table.vue
@@ -6,6 +6,68 @@
     </ul>
   </card>
   <card title="回答内容">
+    <div class="tableHeader">
+      <h3>列 表</h3>
+      <el-button type="primary" @click="addList()">添 加</el-button>
+    </div>
+    <el-table
+      :data="tableData"
+      border
+      stripe
+      style="width: 100%">
+      <el-table-column type="index" label="#" width="60"></el-table-column>
+      <el-table-column prop="name" label="用户名" width="180"></el-table-column>
+      <el-table-column prop="admin" label="isadmin">{{tableData.admin}}</el-table-column>
+      <el-table-column label="用户状态">
+        <template slot-scope="scope">
+        <el-tag type="success">{{scope.row.status == 1? '有效' : '无效'}}</el-tag>
+        </template>
+      </el-table-column>
+      <el-table-column label="创建日期" width="180">
+        <template slot-scope="scope">
+          {{scope.row.create_time}}
+        </template>
+      </el-table-column>
+      <el-table-column  label="操作" width="180">
+        <template slot-scope="scope">
+          <el-button type="text" @click="editList(scope.row)" class="cyan"><i class="el-icon-edit"></i>编 辑</el-button>
+          <el-button type="text" @click="deleteList(scope.row)" class="red"><i class="el-icon-delete"></i>删 除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+     <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="currentPage"
+      :page-sizes="[100, 200, 300, 400]"
+      :page-size="100"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="400">
+    </el-pagination>
+    <el-dialog
+      :title="title"
+      :visible.sync="dialogVisible"
+      width="30%">
+      <el-form :model="form" label-width="100px">
+        <el-form-item label="用户名">
+          <el-input v-model="form.name"></el-input>
+        </el-form-item>
+        <el-form-item label="密码">
+          <el-input v-model="form.password"></el-input>
+        </el-form-item>
+        <el-form-item label="验证密码">
+          <el-input v-model="form.checkPassword"></el-input>
+        </el-form-item>
+        <el-form-item label="是否为管理员">
+          <el-radio v-model="radio" label="1">是</el-radio>
+          <el-radio v-model="radio" label="0">否</el-radio>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="submit">确 定</el-button>
+      </span>
+    </el-dialog>
   </card>
 </div>
 </template>
@@ -16,104 +78,84 @@ import axios from 'axios'
 export default {
   data() {
     return {
+      tableData:[],
+      form: {
+        name: '',
+        password: '',
+        checkPassword: '',
+      },
+      radio: '1',
+      title: '',
+      dialogVisible: false,
+      currentPage: 4
     }
   },
   created(){
-<<<<<<< HEAD
     this.getList()
-    this.getlist1()
-  },
-  mounted() {
   },
   methods: {
-    getlist1(){
-       axios.post('http://129.204.79.16:3000/api/user/query',{admin: 1})
+    getList() {
+      axios.post('http://129.204.79.16:3000/api/user/query',{admin: 0})
       .then((res) => {
-        if(res.data.code === 200){
-          
-        }
+        this.tableData = res.data.data.list
       })
       .catch((res) => {
 
       })
     },
-    getList() {
-      this.loading = true
-      axios.get('/api/data')
-      .then((res) => {
-        if(res.data.code === 200){
-          this.lists = res.data.data
-          this.lists.forEach(element => {
-            element.visible = false
-          });
-          console.log(this.lists)
-          this.loading = false
-          this.init()
-        }
-      })
-      .catch((res) => {
-        console.log(error);
-      })
+    addList() {
+      this.dialogVisible = true
+      this.title = '添加人员'
     },
-    init(){
-     this.list =  this.lists.filter((ele, index) => {
-        if(index >= (this.currentPage - 1) *  this.pageSizes && index < this.currentPage * this.pageSizes) {
-          return  ele
-        }
-      })
+    editList() {
+      this.dialogVisible = true
+      this.title = '编辑人员'
+    },
+    submit() {
+      if(this.title == '添加人员'){
+        const data = this.form
+        axios.post('http://129.204.79.16:3000/api/user/add',data)
+        .then((res) => {
+          if(res.code == 200){
+            this.$message.success(res.msg)
+            this.dialogVisible = false
+          }
+        })
+        .catch((res) => {
+
+        })
+      } else {
+
+      }
+    },
+    deleteList(row) {
+      console.log(row)
     },
     handleSizeChange(val) {
-      // console.log(`每页 ${val} 条`);this
-      this.pageSizes = val
-      this.init()
+      // console.log(`每页 ${val} 条`);
     },
     handleCurrentChange(val) {
       // console.log(`当前页: ${val}`);
-      this.currentPage = val
-      this.init()
-    },
-    statusFilter(a) {
-     return this.statusTags.filter((el) => {
-        return   el.label ===  a
-      })
-    },
-    deleteList(row) {
-      this.list.map((i,index) => {
-       if (row.id === i.id) {
-         this.list.splice(index,1)
-       }
-      })
-    },
-    editList(row) {
-      this.flag = false
-      this.title = '编辑信息'
-      this.dialogVisible =  true
-      let copyRow = {...row}
-      this.form = copyRow
-    },
-    submit(){
-      this.list.map((i,index) => {
-        if (this.form.id === i.id) {
-          this.list.splice(index,1,this.form)
-        }
-      })
-    this.dialogVisible = false
-    },
-    detail(row) {
-      this.flag = true
-      this.title = '详细信息'
-      this.dialogVisible =  true
-      this.form = row
-    },
-    handleSelectionChange(val) {
-    },
-=======
-  },
-  methods: {
->>>>>>> 9d43f854b6b9f77c570f3dfd85f45c6abb6cb082
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+.red{
+  color: #F56C6C;
+}
+.cyan{
+  color: 	#00CED1;
+}
+.tableHeader{
+  padding: 0px 10px 10px 10px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  h3{
+    margin: 0;
+    padding: 0;
+  }
+}
 </style>
