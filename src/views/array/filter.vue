0563b97805a84be72c812ad828460f45dd46934d
Tit1e <evollyone@outlook.com>
2019-07-05 15:02:04 +0800

    解决冲突

--- src/views/array/filter.vue
+++ src/views/array/filter.vue
@@ -4,10 +4,6 @@
     <p>
       <el-link type="primary" target="_blank" href="https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Array/filter">Array.filter()</el-link> 方法的使用
     </p>
-<<<<<<< HEAD
-    <p class="text-danger">过滤出【1995-2010】（包含 1995 与 2010）期间出生的【女生】</p>
-=======
->>>>>>> 4712610b89dcce2f51b23e1de11734f4c8839d64
     <codemirror v-model="source" />
   </card>
 
