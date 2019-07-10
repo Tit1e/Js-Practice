c6dd9a5eeace0d0a19b8681847a02e3c0572e7ff
Tit1e <evollyone@outlook.com>
2019-07-10 10:30:45 +0800

    修改 table 页面描述信息

--- src/views/element/table.vue
+++ src/views/element/table.vue
@@ -2,7 +2,7 @@
 <div>
   <card title="要求">
     <ul>
-      <li>根据接口文档 <strong>ApiDocument.pdf</strong> 中的 【人员管理】部分的接口实现人员的管理功能</li>
+      <li>根据 <router-link to="/apiDocument">API 文档</router-link> 中的 【人员管理】部分的接口实现人员的管理功能</li>
     </ul>
   </card>
   <card title="回答内容">
