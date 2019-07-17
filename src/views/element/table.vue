eb6c6c5c5c20153869cb29f10ce2b58709f78409
Tit1e <evollyone@outlook.com>
2019-07-17 14:35:01 +0800

    bug 反馈

--- src/views/element/table.vue
+++ src/views/element/table.vue
@@ -3,6 +3,7 @@
   <card title="要求">
     <ul>
       <li>根据 <router-link to="/apiDocument">API 文档</router-link> 中的 【人员管理】部分的接口实现人员的管理功能</li>
+      <li class="text-danger">创建时间选择后再清空，js 报错</li>
     </ul>
   </card>
   <card title="回答内容">
